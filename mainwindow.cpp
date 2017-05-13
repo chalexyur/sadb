864ee6534465663e6b7bf9b5034b3fbd468c3bc3
SENSEi <chalexyur@gmail.com>
2017-05-13 18:48:59 +0300

    upd

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "newdialog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +16,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_menuNew_triggered()
 {
+    objNewDialog = new NewDialog(this);
+    objNewDialog->show();
+}
 
+void MainWindow::on_menuExit_triggered()
+{
+    QCoreApplication::exit();
 }
